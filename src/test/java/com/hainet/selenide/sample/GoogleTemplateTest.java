5fb96831ba9f2512ea2a83ad1ff2d14af5a9505a
hainet <20400616+hainet@users.noreply.github.com>
2018-05-17 19:03:32 +0900

    Add a same sample of using SelenideElements.

--- src/test/java/com/hainet/selenide/sample/GoogleTemplateTest.java
+++ src/test/java/com/hainet/selenide/sample/GoogleTemplateTest.java
@@ -1,12 +1,37 @@
 package com.hainet.selenide.sample;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.Before;
 import org.junit.Test;
+
+import static com.codeborne.selenide.Selectors.byCssSelector;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.*;
 
 public class GoogleTemplateTest {
 
+    @Before
+    public void before() {
+        Configuration.browser = WebDriverRunner.CHROME;
+        Configuration.timeout = 3_000L;
+    }
+
     @Test
-    public void searchTest() {
-        final GoogleTemplate template = new GoogleTemplate();
-        template.search("Spring Boot");
+    public void searchFirstByCssSelectorTest() {
+        open("https://www.google.co.jp/");
+        $(byId("lst-ib")).val("site:projects.spring.io Spring Boot").pressEnter();
+        $(byCssSelector("#rso > div > div > div:nth-child(1) > div > div > h3 > a")).click();
+        sleep(3_000);
+        close();
+    }
+
+    @Test
+    public void searchFirstBySelenideElementsTest() {
+        open("https://www.google.co.jp/");
+        $(byId("lst-ib")).val("site:projects.spring.io Spring Boot").pressEnter();
+        $$(byCssSelector("#rso > div > div")).first().$("div > div > h3 > a").click();
+        sleep(3_000);
+        close();
     }
 }
